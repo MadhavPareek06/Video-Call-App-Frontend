a0450db412c9e64236b48846ba90e72a07bbf760
Madhav <madhavpareek06@gmail.com>
2025-07-06 15:24:38 +0530

    Room creation logic with user joining

--- src/Context/SocketContext.tsx
+++ src/Context/SocketContext.tsx
@@ -1,6 +1,8 @@
 import SocketIoClient from "socket.io-client";
-import { createContext, useEffect } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Peer from "peerjs";
+import { v4 as UUIDv4 } from "uuid"; 
 
 
 const WS_Server = "http://localhost:5500";
@@ -18,16 +20,33 @@ interface Props {
 
 export const SocketProvider: React.FC<Props> = ({ children }) => {
   const navigate = useNavigate();
+
+  const [user, setUser] = useState<Peer>(); // new peer user
+  const fetchParticipantList = ({roomId, participants}: {roomId: string, participants: string[]}) => {
+        console.log("Fetched room participants");
+        console.log(roomId, participants);
+    }
+
   useEffect(() => {
+
+     const userId = UUIDv4();
+        const newPeer = new Peer(userId, {
+            host: "localhost",
+            port: 9000,
+            path: "/myapp"
+        });
+        setUser(newPeer);
+
     const enterRoom = ({ roomId} : { roomId: string}) => {
       navigate(`/room/${roomId}`);
     };
     socket.on("room-created", enterRoom);
+    socket.on("get-users", fetchParticipantList);
 
   }, []);
 
   return (
-    <SocketContext.Provider value={socket}>
+    <SocketContext.Provider value={{ socket, user }}>
       {children}
     </SocketContext.Provider>
   );
