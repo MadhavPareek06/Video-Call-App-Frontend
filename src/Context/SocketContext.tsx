95c805a240a1476c6e0979ab790d88cbf9934f1f
Madhav <madhavpareek06@gmail.com>
2025-07-06 15:50:03 +0530

    added user feed

--- src/Context/SocketContext.tsx
+++ src/Context/SocketContext.tsx
@@ -22,10 +22,18 @@ export const SocketProvider: React.FC<Props> = ({ children }) => {
   const navigate = useNavigate();
 
   const [user, setUser] = useState<Peer>(); // new peer user
+  const [stream, setStream] = useState<MediaStream>();
+
+
   const fetchParticipantList = ({roomId, participants}: {roomId: string, participants: string[]}) => {
         console.log("Fetched room participants");
         console.log(roomId, participants);
     }
+
+  const fetchUserFeed = async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true});
+        setStream(stream);
+    } 
 
   useEffect(() => {
 
@@ -36,6 +44,7 @@ export const SocketProvider: React.FC<Props> = ({ children }) => {
             path: "/myapp"
         });
         setUser(newPeer);
+        fetchUserFeed();
 
     const enterRoom = ({ roomId} : { roomId: string}) => {
       navigate(`/room/${roomId}`);
@@ -46,7 +55,7 @@ export const SocketProvider: React.FC<Props> = ({ children }) => {
   }, []);
 
   return (
-    <SocketContext.Provider value={{ socket, user }}>
+    <SocketContext.Provider value={{ socket, user,stream }}>
       {children}
     </SocketContext.Provider>
   );
